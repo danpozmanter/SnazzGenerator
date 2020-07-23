7c4d04c77a2768d6709a803c76e68a4baf3e2aec
FatherFoxxy <rattusunison@gmail.com>
2020-07-22 21:25:34 -0700

    Minor formatting adjustment, use generic arguments over type instance, use typeof<T> when appropriate

--- src/Library.fs
+++ src/Library.fs
@@ -6,38 +6,37 @@ module SnazzGen =
     open System.Text
     open System.Text.RegularExpressions
     open System.Reflection
-    
-    exception SnazzGenTypeError of string
-    
+
     type SnazzMeta =
         | Meta of PrimaryKey:string
         | MetaSetBytea of PrimaryKey:string
         | MetaSetTable of PrimaryKey:string * TableName:string
         | MetaSetTableBytea of PrimaryKey:string * TableName:string
-    
+
     let CamelCasePattern = Regex(@"[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+")
     let ValuesClausePattern = Regex(@"(?<= VALUES ).+")
 
     let transformDotnetNameToSQL (propertyName:string) =
         String.Join("_", CamelCasePattern.Matches(propertyName)).ToLower()
 
-    let getValueFromProperty (property:PropertyInfo) (bytea:bool) = 
-            if bytea && (property.PropertyType = Type.GetType("System.Byte[]")) then
+    let getValueFromProperty (property:PropertyInfo) (bytea:bool) =
+            if bytea && (property.PropertyType = typeof<Byte[]>) then
                 "@" + property.Name + "::bytea"
             else
                 "@" + property.Name
 
-    let setMeta (typeInstance:Type) (meta:SnazzMeta) =
+    let setMeta<'Type> (meta:SnazzMeta) =
+        let typeInstance = typeof<'Type>
         match meta with
             | MetaSetTable(key, table) -> (key, table, false)
             | MetaSetTableBytea(key, table) -> (key, table, true)
             | Meta(key) -> (key, (transformDotnetNameToSQL typeInstance.Name), false)
             | MetaSetBytea(key) -> (key, (transformDotnetNameToSQL typeInstance.Name), true)
-    
-    let buildInsert (typeInstance:Type) (meta:SnazzMeta) =
-        if (typeInstance = null) then
-            raise (SnazzGenTypeError "Error: Type provided to mapper is null.")
-        let primaryKey, tableName, bytea = setMeta typeInstance meta
+
+    let buildInsert<'Type> (meta:SnazzMeta) =
+        let typeInstance = typeof<'Type>
+
+        let primaryKey, tableName, bytea = setMeta<'Type> meta
         let props = typeInstance.GetProperties()
         let statement = StringBuilder()
         let statement = statement.Append("INSERT INTO " + tableName)
@@ -47,13 +46,15 @@ module SnazzGen =
             if prop.Name <> primaryKey then
                 fields.Add(transformDotnetNameToSQL prop.Name)
                 values.Add(getValueFromProperty prop bytea)
-        let statement = statement.Append(" (")
-        let statement = statement.Append(String.Join(", ", fields))
-        let statement = statement.Append(") VALUES (")
-        let statement = statement.Append(String.Join(", ", values))
-        let statement = statement.Append(")")
-        statement.ToString()
-    
+
+        statement
+            .Append(" (")
+            .Append(String.Join(", ", fields))
+            .Append(") VALUES (")
+            .Append(String.Join(", ", values))
+            .Append(")")
+        |> string
+
     let makeInsertBulk insertStatement rows =
         let valuesClause = ValuesClausePattern.Match(insertStatement).Value
         insertStatement + ", " + String.Join(", ", List.replicate (rows - 1) valuesClause) + ";"
